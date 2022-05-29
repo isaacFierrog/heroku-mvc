58fcca457b61b3103facbd03dc4e1dc1c5e544a1
Isaac Fierro <zifg_36@live.com.mx>
2022-05-29 14:53:05 -0700

    Segundo commit

--- app.py
+++ app.py
@@ -11,5 +11,10 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/otra-pagina')
+def otra_pagina():
+    return render_template('nueva-pagina.html')
+
+
 if __name__ == '__main__':
     app.run()
